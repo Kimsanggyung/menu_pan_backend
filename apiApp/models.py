4de4ab3a4e548d36e5c75453bb65cffa4a0d22d9
Kim sanggyung <ksang0906@gmail.com>
2022-10-06 17:39:19 +0900

    이름변경

--- apiApp/models.py
+++ apiApp/models.py
@@ -1,6 +1,6 @@
 from turtle import update
 from django.db import models
 
-class menu(models.Model):
+class Menu(models.Model):
   menuName = models.TextField()
   update_at = models.DateTimeField(auto_now=True)
