ad7e4c9f534b85fa60c7d7042d11a1669774d99f
Kim sanggyung <ksang0906@gmail.com>
2022-10-28 14:09:32 +0900

    캐쉬 정리

--- apiApp/models.py
+++
@@ -1,6 +0,0 @@
-from turtle import update
-from django.db import models
-
-class Menu(models.Model):
-  menuName = models.TextField()
-  update_at = models.DateTimeField(auto_now=True)
--- apiApp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Menu
-
-class MenuSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Menu
-    fields = ('id', 'menuName', 'update_at')
